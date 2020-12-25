05345d44e0c08f8ebcfc7b87561301c227c239b9
chanukaHub <chanukadil1999@gmail.com>
2020-12-26 04:52:51 +0530

    finished receptionistPostalEditDispatchedPostal function

--- src/lk/usj/OPD_Management/java/controller/receptionist/postal/ReceptionistPostalEditDispatchedPostalController.java
+++ src/lk/usj/OPD_Management/java/controller/receptionist/postal/ReceptionistPostalEditDispatchedPostalController.java
@@ -4,9 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,10 +20,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import lk.usj.OPD_Management.java.common.Common;
 import lk.usj.OPD_Management.java.dto.PostalDTO;
+import lk.usj.OPD_Management.java.service.custom.PostalBO;
+import lk.usj.OPD_Management.java.service.custom.impl.PostalBOImpl;
 
 public class ReceptionistPostalEditDispatchedPostalController implements Initializable {
     final String postalType="Dispatched";
+    private PostalBO postalBO=new PostalBOImpl();
+    String selectedFilePath="",currentFilePath;
+    String path;
 
     @FXML
     private ResourceBundle resources;
@@ -88,7 +101,19 @@ public class ReceptionistPostalEditDispatchedPostalController implements Initial
 
     @FXML
     void postalAttachmentBtn_OnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select PDF files");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("TEXT Files", "*.txt"));
 
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            postalAttachmentLbl.setText(selectedFile.getPath());
+            selectedFilePath = selectedFile.getPath();
+        }
+        else {
+            postalAttachmentLbl.setText("File selection cancelled.");
+        }
     }
 
     @FXML
@@ -98,6 +123,67 @@ public class ReceptionistPostalEditDispatchedPostalController implements Initial
 
     @FXML
     void saveBtn_OnAction(ActionEvent event) throws Exception{
+        try{
+
+            if (postalReferenceNOTxt.getText().equals("")){
+                Common.showError("Can not found reference No !");
+                return;
+            }else if(fromAddressTxt.getText().equals("")){
+                Common.showError("Please Enter from Address ");
+                return;
+            }else if(toAddressTxt.getText().equals("")){
+                Common.showError("Please Enter to Address ");
+                return;
+            }else if(fromNameTxt.getText().equals("")){
+                Common.showError("Please Enter from Name ");
+                return;
+            }else if(toNameTxt.getText().equals("")){
+                Common.showError("Please Enter to Name ");
+                return;
+            }
+            LocalDate ld = postalDateDatePicker.getValue();
+            Calendar c =  Calendar.getInstance();
+            if (ld == null){
+                Common.showError("Please Enter Date");
+                return;
+            }
+            c.set(ld.getYear(), ld.getMonthValue() - 1, ld.getDayOfMonth());
+            Date date = c.getTime();
+
+            Date today= new Date();
+
+            if (selectedFilePath.equals("")){
+                path = currentFilePath;
+            }else {
+                File newFile1 = new File("AttachmentDocumentsStorage\\"+postalReferenceNOTxt.getText()+String.valueOf(today.getDate())+String.valueOf(today.getMinutes())+String.valueOf(today.getSeconds())+toNameTxt.getText()+fromAddressTxt.getText()+".txt");
+                Files.copy(Path.of(selectedFilePath),newFile1.toPath());
+                path = newFile1.getPath();
+            }
+
+
+            PostalDTO postalDTO= new PostalDTO(
+                    postalReferenceNOTxt.getText(),
+                    fromAddressTxt.getText(),
+                    toAddressTxt.getText(),
+                    fromNameTxt.getText(),
+                    toNameTxt.getText(),
+                    date,
+                    path,
+                    postalNotesTxtArea.getText(),
+                    postalType
+            );
+
+            boolean b = postalBO.updatePostal(postalDTO);
+
+            if (b){
+                Common.showMessage("Successfully Updated!");
+            }
+            else
+                Common.showError("Not added");
+        } catch (Exception e1) {
+            Common.showError("Not added");
+            e1.printStackTrace();
+        }
 
         ((Node)(event.getSource())).getScene().getWindow().hide();
 
@@ -130,5 +216,7 @@ public class ReceptionistPostalEditDispatchedPostalController implements Initial
         postalDateDatePicker.setValue(localDate);
         postalAttachmentLbl.setText(postalDTO.getAttachment());
         postalNotesTxtArea.setText(postalDTO.getNotes());
+
+        currentFilePath= postalDTO.getAttachment();
     }
 }
